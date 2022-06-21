de7d5a587d47e5780699979121790cbdf626b7f4
Pierre Etrillard <pierre.etrillard@live.fr>
2022-06-21 11:04:35 +0200

    optimisation du code (suppression de la fonction'productList()' et integration de la boucle 'for' au sein de fetch

---
+++ front/js/script.js
@@ -0,0 +1,48 @@
+/**
+ * Send request to the api for obtain the arrays af product
+ * @param { String } url
+ * @param { String } method
+ * @param { Object } body
+ * @return { Promise }
+ */
+/*Les fiches produits à récupérer auprès de l'api sont contenu dans un tableau d'objets
+ et ont la structure suivante: {
+   colors: Array(),
+   _id: 'string',
+   name: 'string',
+   price: number,
+   imageUrl: 'url',
+   altTxt: 'string',
+   description:'string'
+  }*/
+
+  //requéte des produits à l'api
+  fetch("http://localhost:3000/api/products")
+    .then((res) =>{
+      //traitement de la réponse si ok = capte la réponse sous forme d'un objet .json
+      return res.ok ? res.json()
+        : alert("Catalogue indisponible, merci de vous connecter ulterieurement.");
+    })
+    //      EXTRACTION DES DONNÉES
+    .then((value) => {
+      // "i" prendra la valeur de chaque indice d'objets du tableau fournis par l'api
+      for (let i in value){
+      //    CREATION D'UNE "<div id='(valeur de l'id du produit visé)'>"  //
+      let newCard = document.createElement("div");
+      newCard.setAttribute("id", `${value[i]._id}`);
+    
+      // INTEGRATION DU NOUVEL ELEMENT DANS LE DOM
+      let itemWrapper = document.getElementById("items");
+      itemWrapper.appendChild(newCard).innerHTML =
+      `<a href="./product.html?id=${value[i]._id}">
+          <article>
+            <img src="${value[i].imageUrl}" alt="${value[i].altTxt}", ${value[i].name}">
+            <h3 class="productName">${value[i].name}</h3>
+            <p class="productDescription">${value[i].description}</p>
+          </article>
+        </a>`};
+    })
+    .catch((err) =>{
+      return null ? console.log("une erreur s'est produite lors du chargement des fiches produit")
+        : console.log('les fiches produits ont été correctement chargées');;
+    });
