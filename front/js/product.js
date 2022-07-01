c6b4fa13d4932dd43260f4423d262af9fa7770b2
Pierre Etrillard <pierre.etrillard@live.fr>
2022-07-01 12:00:09 +0200

    avancée dans le remplissage du pannier ( reste à circoncire le total d'articles à 100 avant envoi au localstorage)

--- front/js/product.js
+++ front/js/product.js
@@ -14,7 +14,7 @@ fetch(`http://localhost:3000/api/products/${productId}`)
   })
   //      EXTRACTION DES DONNÉES & IMPLÉMENTATION DE CELLES-CI DANS LE DOM
   .then((value) => {
-// console.log(value);
+    // console.log(value);
     document.querySelector(
       "div.item__img"
     ).innerHTML = `<img src="${value.imageUrl}" alt="photographie du modéle ${value.name}">`;
@@ -31,77 +31,84 @@ fetch(`http://localhost:3000/api/products/${productId}`)
       // console.log(value.colors.length)
     }
   })
+
   .catch((err) => {
     console.log(
       "une erreur s'est produite lors du traitement de la fiche produit",
-      + err
+      +err
     );
   });
 
-/*  REMPLISSAGE DU PANNIER AVEC L'ID, LA COULEUR ET LA QUANTITÉ DU PRODUIT À COMMANDE 
-OU AVEC LES DONNÉES PRÉCEDEMMENT SAISIE (contenu dans le localstorage)*/
 const colorChoice = document.getElementById("colors");
+console.log("input:colors", colorChoice, colorChoice.value);
 const quantity = document.getElementById("quantity");
-let cart = localStorage.cart ?  JSON.parse(localStorage.cart):[]
-const similarIdStored = cart.find(elt=>elt.id === `${productId}`)
-const similarColorStored = cart.find(elt=>elt.color === `${colorChoice.value}`)
-// const cartElements = cart.find( elmt => elmt.color === `${colorChoice.value}`)
+console.log("input:quantity", quantity, quantity.value);
 
-
-console.log(
-  `avant le click localStorage contient: ${localStorage} & cart: ${cart}, de plus ${quantity.value} produits ${colorChoice.value} ont été sélectionné`
-);
+let cart = localStorage.cart ? JSON.parse(localStorage.cart) : [];
+console.log("cart", cart);
 
 document.getElementById("addToCart").addEventListener("click", () => {
-    if (colorChoice.value == "") {
+  /*  REMPLISSAGE DU PANNIER AVEC L'ID, LA COULEUR ET LA QUANTITÉ DU PRODUIT À COMMANDE 
+  OU AVEC LES DONNÉES PRÉCEDEMMENT SAISIE (contenu dans le localstorage)*/
+  const chosenColor = colorChoice.value;
+  const chosenQuantity = quantity.value;
+  console.log(`onClick - color: ${chosenColor}`);
+  console.log(`onClick - quantity: ${chosenQuantity}`);
+  console.log(`onClick - card: ${cart}`);
+
+  if (colorChoice.value == "") {
     alert("veuillez séléctionner la couleur de votre canapé.");
   } else if (Number(quantity.value) < 1 || Number(quantity.value) > 100) {
     alert("veuillez saisir un nombre d'article entre 1 et 100.");
-  }  
-    else if (similarIdStored && similarColorStored ){
-      elt.amount = elt.amount + `${Number(quantity.value)}`
+  } else {
+    const similarIdStored = cart.find((elt) => elt.id === `${productId}`);
+    console.log("similarId", productId, similarIdStored);
+    const similarColorStored = cart.find(
+      (elt) => elt.color === `${colorChoice.value}`
+    );
+    console.log("similarColor", colorChoice.value, similarColorStored);
+    const similarProductStored = similarColorStored && similarIdStored;
+
+    console.log(`onClick - similarIdStored: ${similarIdStored}`);
+    console.log(`onClick - similarColorStored: ${similarColorStored}`);
+
+    if (cart.length < 1 || typeof similarIdStored === `undefined`) {
+      cart.push({
+        id: `${productId}`,
+        color: `${colorChoice.value}`,
+        amount: `${Number(quantity.value)}`,
+      });
+      console.log(
+        `Création d'un nouvel objet au tableau cart contenant ceci: ${cart}`
+      );
+    } else if (
+      similarIdStored.id == `${productId}` &&
+      typeof similarColorStored === `undefined`
+    ) {
+      {
+        cart.push({
+          id: `${productId}`,
+          color: `${colorChoice.value}`,
+          amount: `${Number(quantity.value)}`,
+        });
+        console.log(`ajout le l'objet au tableau cart contenant ceci: ${cart}`);
+      }
+    } else if (
+      similarIdStored.id == `${productId}` &&
+      similarColorStored.color == `${colorChoice.value}`
+    ) {
+      similarColorStored.amount =
+        Number(similarColorStored.amount) + Number(quantity.value);
+
+      console.log(`Mise à jour du tableau cart contenant ceci: ${cart}`);
     }
-  else {cart.push({
-    id: `${productId}`,
-    color: `${colorChoice.value}`,
-    amount: `${Number(quantity.value)}`,
-   });
-   console.log(`Mise à jour du tableau cart contenant ceci: ${cart}`);
   }
-//  VERIFIE LA SELECTION DES VALEURS ET COULEURS
+//    Verifier si la commande ne dépasse pas les 100 article avant l'envoi au localstorage
+  console.log(`après le click, cart contient: ${cart}`);
 
-//  VERIFIE SI LE PANIER EST VIDE SINON CRÉATION DE CELUI-CI
-//   else if (cart == undefined) {
-//     cart = [{
-//      id: `${productId}`,
-//      color: `${colorChoice.value}`,
-//      amount: `${Number(quantity.value)}`,
-//     }];
-//     
-//   } 
-// // VERIFIE SI OBJET DE MÊME COULEUR EST PRESENT DANS LA VARIABLE
-//   else if (cart.find( clr => clr.color === `${colorChoice.value}`)){
-//     cart[0].amount = Number(cart.amount) + Number(quantity.value);
-//     cart.forEach(element => {
-//       console.log("éléments présents dans cart"+ element)
-//     });
-//   }
-//   else if (cart.find( clr => clr.color != `${colorChoice.value}`)){
-//     cart.forEach(element => {
-//       console.log("éléments présents dans cart"+ element)
-//     });
-//   }
-// 
-console.log(
-  `avant le click cart contient: ${cart}`); 
+  cartStringed = JSON.stringify(cart);
+  console.log("après le clique, cartStringed = " + cartStringed);
 
- cartStringed = JSON.stringify(cart);
-  console.log(
-    "après le clique, cartStringed = " + cartStringed
-  );
-  
-  localStorage.cart = cartStringed
-  console.log(
-    "après le clique, localStorage.cart  = " + localStorage.cart
-  );
+  localStorage.cart = cartStringed;
+  console.log("après le clique, localStorage.cart  = " + localStorage.cart);
 });
